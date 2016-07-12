f48224b5539e7ca9a8568f1c6a4295b2624f468f
Ashiq <ashiq.r31@gmail.com>
2016-07-13 03:26:08 +0400

    port code changed

--- app.js
+++ app.js
@@ -93,7 +93,17 @@ router.get('/results', function(req, res){
 });
 
 app.use('/', router);
+//
+// app.listen(5000, function(){
+//   console.log('Sushi Hunt is listening on port 5000!');
+// });
 
-app.listen(5000, function(){
-  console.log('Sushi Hunt is listening on port 5000!');
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
