5a40a3b1b5f40058efa5caf0e525fb72abccbd0c
Ashiq <ashiq.r31@gmail.com>
2016-07-15 11:53:06 +0400

    new fetch request added

--- public/sw1.js
+++ public/sw1.js
@@ -23,16 +23,39 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event){
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response){
-      if(response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+// self.addEventListener('fetch', function(event){
+//   event.respondWith(
+//     caches.match(event.request)
+//     .then(function(response){
+//       if(response) {
+//         return response;
+//       }
+//       return fetch(event.request);
+//     })
+//   );
+// });
+
+var jsonDataRe = /\/feed/i;
+
+self.addEventListener('fetch', function(event) {
+    var request = event.request,
+        match = jsonDataRe.exec(request.url);
+
+    if (match) {
+        // Use regex capturing to grab only the bit of the URL
+        // that we care about, ignoring query string, etc.
+        var cacheRequest = new Request(match[1]);
+        event.respondWith(
+            caches.match(cacheRequest).then(function(response) {
+                return response || fetch(request).then(function(response) {
+                    caches.open('sushi-v7').then(function(cache) {
+                      cache.put(cacheRequest, response);
+                    })
+                    return response;
+                });
+            })
+        );
+    }
 });
 
 self.addEventListener('activate', function(event) {
