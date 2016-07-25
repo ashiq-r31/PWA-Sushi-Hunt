5ffccd8699d102580e1d44a7c8fa4e3642b2a0f1
Ashiq <ashiq.r31@gmail.com>
2016-07-25 22:05:48 +0400

    scripts rearranged

--- public/javascripts/main2.js
+++ public/javascripts/main2.js
@@ -7,3 +7,23 @@ if ('serviceWorker' in navigator) {
     console.log('ServiceWorker registration failed: ', err);
   });
 }
+
+// function urlParam(){
+//     var hashes = window.location.href.slice(window.location.href.indexOf('=') + 1).split('&');
+//     return hashes;
+// }
+
+// function urlParam(){
+//   var hashes = $('[name="budget"]').val();
+//   return hashes;
+// }
+$('#submit').click(function(event){
+  var budget = $('[name="budget"]').val();
+
+  console.log(budget + " is your budget amount");
+
+  $.get('/results', {budget:budget}, function(data){
+    $('#loader').hide();
+    $('#results').html(data);
+  });
+});
--- public/sw1.js
+++ public/sw1.js
@@ -8,12 +8,12 @@ var urlsToCache_ = [
   '/javascripts/main2.js'
 ];
 
-version = 'v2';
+version = 'v3';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Installed version', version);
   event.waitUntil(
-    caches.open('sushi-v2')
+    caches.open('sushi-v3')
       .then(function(cache) {
       console.log("opened cache");
       return cache.addAll(urlsToCache_);
@@ -77,13 +77,13 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['sushi-v2'];
+  var cacheWhitelist = ['sushi-v3'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if ('sushi-v2' && cacheWhitelist.indexOf(cacheName) === -1) {
+          if ('sushi-v3' && cacheWhitelist.indexOf(cacheName) === -1) {
             console.log('Deleted old cache');
             return caches.delete(cacheName);
           }
