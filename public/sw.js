ab6de5b9c21dfb13561df1b7d70a0f84d70b3062
Ashiq <ashiq.r31@gmail.com>
2016-08-23 02:06:37 +0400

    changed sw version

--- public/sw.js
+++ public/sw.js
@@ -16,12 +16,12 @@ var urlsToCache_ = [
   '/javascripts/main.js'
 ];
 
-version = 'v5';
+version = 'v5.5';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Installed version', version);
   event.waitUntil(
-    caches.open('sushi-v5')
+    caches.open('sushi-v5.5')
       .then(function(cache) {
       console.log("opened cache");
       return cache.addAll(urlsToCache_);
@@ -40,13 +40,13 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['sushi-v5'];
+  var cacheWhitelist = ['sushi-v5.5'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if ('sushi-v5' && cacheWhitelist.indexOf(cacheName) === -1) {
+          if ('sushi-v5.5' && cacheWhitelist.indexOf(cacheName) === -1) {
             console.log('Deleted old cache');
             return caches.delete(cacheName);
           }
