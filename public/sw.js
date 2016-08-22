4189fb0699126d83e81e55a599bf65385ff76e39
Ashiq <ashiq.r31@gmail.com>
2016-08-23 03:08:40 +0400

    last time

--- public/sw.js
+++ public/sw.js
@@ -10,15 +10,15 @@ var urlsToCache_ = [
   '/images/detect.svg',
   '/images/sushi-196.png',
   '/javascripts/main.js',
-  '/javascripts/manifest.json'
+  '/manifest.json'
 ];
 
-version = 'v1';
+version = 'v4';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Installed version', version);
   event.waitUntil(
-    caches.open('sushi-v1')
+    caches.open(version)
       .then(function(cache) {
       console.log("opened cache");
       return cache.addAll(urlsToCache_);
@@ -37,13 +37,13 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['sushi-v1'];
+  var cacheWhitelist = [version];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if ('sushi-v1' && cacheWhitelist.indexOf(cacheName) === -1) {
+          if (version && cacheWhitelist.indexOf(cacheName) === -1) {
             console.log('Deleted old cache');
             return caches.delete(cacheName);
           }
