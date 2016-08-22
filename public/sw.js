73a008ade30b6f6c6b679edc98f597897d991c41
Ashiq <ashiq.r31@gmail.com>
2016-08-23 01:55:45 +0400

    new icons added

--- public/sw.js
+++ public/sw.js
@@ -10,16 +10,20 @@ var urlsToCache_ = [
   '/images/detect.svg',
   '/images/direction.svg',
   '/images/sushi-48.png',
+  '/images/sushi-72.png',
+  '/images/sushi-96.png',
+  '/images/sushi-144.png',
+  '/images/sushi-168.png',
   '/images/sushi-192.png',
   '/javascripts/main.js'
 ];
 
-version = 'v4';
+version = 'v5';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Installed version', version);
   event.waitUntil(
-    caches.open('sushi-v4')
+    caches.open('sushi-v5')
       .then(function(cache) {
       console.log("opened cache");
       return cache.addAll(urlsToCache_);
@@ -38,13 +42,13 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['sushi-v4'];
+  var cacheWhitelist = ['sushi-v5'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if ('sushi-v4' && cacheWhitelist.indexOf(cacheName) === -1) {
+          if ('sushi-v5' && cacheWhitelist.indexOf(cacheName) === -1) {
             console.log('Deleted old cache');
             return caches.delete(cacheName);
           }
